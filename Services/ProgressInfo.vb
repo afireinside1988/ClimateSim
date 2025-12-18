8b53c1f494e48a02908d9d6503156e20f0799248
afireinside1988 <robinharazim1988@live.de>
2025-12-18 11:14:46 +0100

    - ProgressOverlay-Fix für Anzeige bei Indeterminate-Progress

--- Services/ProgressInfo.vb
+++ Services/ProgressInfo.vb
@@ -1,9 +1,16 @@
 ﻿Public Class ProgressInfo
+
+    Public Const Indeterminate As Integer = -1
     Public Property Message As String
-    Public Property Percent As Integer '0..100
+    Public Property Percent As Integer '-1 (indeterminate) oder 0..100
 
     Public Sub New(message As String, percent As Integer)
         Me.Message = message
-        Me.Percent = Math.Max(0, Math.Min(100, percent))
+
+        If percent < 0 Then
+            Me.Percent = Indeterminate
+        Else
+            Me.Percent = Math.Max(0, Math.Min(100, percent))
+        End If
     End Sub
 End Class
